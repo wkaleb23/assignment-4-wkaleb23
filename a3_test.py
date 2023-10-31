95691004d2331a655ca51d61551470291f126a3d
wkaleb23 <wkaleb23@uw.edu>
2023-10-31 00:39:58 -0700

    trying to fix unittests a3

--- a3_test.py
+++ a3_test.py
@@ -2,14 +2,32 @@ import sys
 import unittest
 from a3 import *
 
+
 class AllTests(unittest.TestCase):
-    def create_deck_test():
+
+    def test_push_card_to_deck(self):
+        # test that you can't add a card to a full deck, or a card that is already in the deck
+        ## Set up
+        deck = create_deck()
+        card = ("jack", "hearts", 11)
+        fake_card = ("test", "hearts", 16)
+        ## Execute the thing to test
+        full_deck = push_card_to_deck(deck, fake_card)
+        removed_card = pop_card(deck)
+        card_already_in_deck = push_card_to_deck(deck, card)
+        add_card = push_card_to_deck(deck, removed_card)
+        ## Evaluate the result for correctness
+        assert full_deck is deck, "Deck should be full"
+        assert card_already_in_deck is deck, "Card should already be in deck"
+        assert add_card is deck, "Card should be added to deck"
+        print("push_card_to_deck_test passed")
+
+    def test_create_deck(self):
         deck = create_deck()
         print_deck(deck)
         print("Create deck test passed")
 
-
-    def create_card_node_test():
+    def test_create_card_node(self):
         card_node = create_card_node("my_payload")
         ## Because this is a unit test for "create_card_node()",
         ##  I'm willing to "break the abstraction" to make sure
@@ -22,19 +40,16 @@ class AllTests(unittest.TestCase):
             print("Test failed-- wrong next node")
         print("Create card_node test passed")
 
-
-    def card_node_next_test():
+    def test_card_node_next(self):
         print("Create card_node_next test by-passed")
 
-
-    def create_hand_test():
+    def test_create_hand(self):
         hand = create_hand()
         assert hand['first_card'] is None, "First card is not None when it should be"
         assert hand['num_cards_in_hand'] == 0, "num_cards_in_hand is not 0 when it should be"
         print("create_hand_test passed")
 
-
-    def add_1card_to_hand_test():
+    def test_add_1card_to_hand(self):
         ## Set up
         hand = create_hand()
         card = ("jack", "hearts", 10)
@@ -46,8 +61,7 @@ class AllTests(unittest.TestCase):
             hand['first_card']) is card, 'The first card is not the right one'
         print("add_card_to_hand_test passed")
 
-
-    def add_2cards_to_hand_test():
+    def test_add_2cards_to_hand(self):
         ## Set up
         hand = create_hand()
         card = ("jack", "hearts", 11)
@@ -62,8 +76,7 @@ class AllTests(unittest.TestCase):
         assert get_card_from_node(hand['first_card']) is card2, "Card2 is not the first card in the hand, but should be"
         print("add_2card_to_hand_test passed")
 
-
-    def remove_first_card_from_hand_test():
+    def test_remove_first_card_from_hand(self):
         ## Set up
         hand = create_hand()
         card = ("jack", "hearts", 11)
@@ -80,8 +93,7 @@ class AllTests(unittest.TestCase):
         assert get_card_from_node(hand['first_card']) is card2, "Card 2 is not the first element but should be"
         print("remove_first_card_from_hand_test passed")
 
-
-    def remove_last_card_from_hand_test():
+    def test_remove_last_card_from_hand(self):
         ## Set up
         hand = create_hand()
         card = ("jack", "hearts", 11)
@@ -98,8 +110,7 @@ class AllTests(unittest.TestCase):
         assert get_card_from_node(hand['first_card']) is card, "Card 1 is not the first element but should be"
         print("remove_last_card_from_hand_test passed")
 
-
-    def check_full_hand_test():
+    def test_check_full_hand(self):
         # try to add a card to a full hand
         ## Set up
         hand = create_hand()
@@ -123,53 +134,17 @@ class AllTests(unittest.TestCase):
         print("check_full_hand_test passed")
 
 
-    def deck_functions_test():
-        # test the functions peek_card, pop_card, is_deck_empty
-        ## Set up
-        deck = create_deck()
-        card = ("jack", "hearts", 11)
-        card2 = ("queen", "hearts", 12)
-        card3 = ("king", "hearts", 13)
-        ## Execute the thing to test
-        top_card_peek = peek_card(deck)
-        top_card_pop = pop_card(deck)
-        is_empty = is_deck_empty(deck)
-        ## Evaluate the result for correctness
-        assert top_card_peek is card3, "Top card should be king"
-        assert top_card_pop is card3, "Top card should be king"
-        assert is_empty == 0, "Deck should not be empty"
-        print("deck_functions_test passed")
-
-
-    def push_card_to_deck_test():
-        # test that you can't add a card to a full deck, or a card that is already in the deck
-        ## Set up
-        deck = create_deck()
-        card = ("jack", "hearts", 11)
-        fake_card = ("test", "hearts", 16)
-        ## Execute the thing to test
-        full_deck = push_card_to_deck(deck, fake_card)
-        removed_card = pop_card(deck)
-        card_already_in_deck = push_card_to_deck(deck, card)
-        add_card = push_card_to_deck(deck, removed_card)
-        ## Evaluate the result for correctness
-        assert full_deck is deck, "Deck should be full"
-        assert card_already_in_deck is deck, "Card should already be in deck"
-        assert add_card is deck, "Card should be added to deck"
-        print("push_card_to_deck_test passed")
-
-
-    def main():
-        create_deck_test()
-        create_card_node_test()
-        card_node_next_test()
-        create_hand_test()
-        add_1card_to_hand_test()
-        add_2cards_to_hand_test()
-        remove_first_card_from_hand_test()
-        remove_last_card_from_hand_test()
-        check_full_hand_test()
-        push_card_to_deck_test()
+    # def main(self):
+    #     test_create_deck()
+    #     test_create_card_node()
+    #     test_card_node_next()
+    #     test_create_hand()
+    #     test_add_1card_to_hand()
+    #     test_add_2cards_to_hand()
+    #     test_remove_first_card_from_hand()
+    #     test_remove_last_card_from_hand()
+    #     test_check_full_hand()
+    #     test_push_card_to_deck()
 
 
 if __name__ == '__main__':
